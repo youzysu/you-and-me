3b6e1aed09ccc20db8ecd6453a8d4559a7cc35ad
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-24 20:33:30 +0900

    :sparkles: LogOut

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -3,17 +3,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "routes/Home"
 import Auth from "routes/Auth"
 import SignUp from "routes/SignUp"
+import Profile from "routes/Profile"
+import Navigation from 'components/Navigation';
 
 function AppRouter({ isLoggedIn }) {
 
   return (
     <BrowserRouter>
+    {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
+          <>
           <Route
             path="/"
             element={<Home />}
           />
+          <Route 
+            path="/profile"
+            element={<Profile />}
+          />
+          </>
         ) : (
           <>
           <Route 
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { authService } from "fbase"
+import { useNavigate } from "react-router-dom"
 
 function Profile() {
+    const Navigate = useNavigate();
+    const onLogoutClick = () => {
+        authService.signOut();
+        Navigate("/")
+    }
+
     return (
         <div>
-            Profile
+            <button onClick={onLogoutClick}>로그아웃</button>
         </div>
     )
 }
